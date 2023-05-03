17ee4d5255ad0404c772a9f0017ecf8a025313a9
MinjiYun <minjiyun204@gmail.com>
2023-05-03 16:07:03 +0900

    chore: husky 적용 #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Test from '@components/Test';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,16 +7,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Test />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
